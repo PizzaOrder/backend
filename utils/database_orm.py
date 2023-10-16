0a5f7d097c19344f23e6fd4d7f9cdc20b5f0734f
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-10-16 17:51:19 +0300

    feat(sqlalchemy): add engine

---
+++ utils/database_orm.py
@@ -0,0 +1,13 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# dialect+driver://username:password@host:port/database
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres@localhost:5432/PizzaOrder'
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
