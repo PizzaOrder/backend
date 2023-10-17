f4179eabedcef8198f6a11c51165e9eb45c262a7
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-10-17 18:53:44 +0300

    refactor(pizza): rename from default_pizza to pizza

--- models/pizzas.py
+++ models/pizzas.py
@@ -2,8 +2,8 @@ from sqlalchemy import Column, Float, Integer, String
 from utils.database_orm import Base
 
 
-class DefaultPizza(Base):
-    __tablename__ = "default_pizzas"
+class Pizza(Base):
+    __tablename__ = "pizzas"
 
     pizza_id = Column(Integer, primary_key=True)
     name = Column(String)
