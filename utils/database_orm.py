6ef697f914865f4b5a89c2caac9beb98c6c611a2
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-11-08 14:55:08 +0300

    fix: add password from db to database_orm

--- utils/database_orm.py
+++ utils/database_orm.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 # dialect+driver://username:password@host:port/database
-SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://admin@localhost:5432/PizzaOrder"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://admin:root@localhost:5432/PizzaOrder"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
 
