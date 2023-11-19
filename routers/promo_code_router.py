79226db5f955e1cce6bb452206b2946953bc30a0
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-11-19 17:23:48 +0300

    feat: add router to get all codes

--- routers/promo_code_router.py
+++ routers/promo_code_router.py
@@ -3,10 +3,10 @@ from typing import Annotated
 from fastapi import APIRouter, Depends, Path
 from sqlalchemy.orm import Session
 
-from crud.promo_code_crud import promo_code_exists
+from crud.promo_code_crud import promo_code_exists, get_all_promo_codes
 from utils.get_db import get_db
 
-router = APIRouter(prefix="/promo_code", tags=["promo code"])
+router = APIRouter(prefix="/promo_codes", tags=["promo code"])
 
 
 @router.get("validate/{promo_code}")
@@ -14,3 +14,8 @@ def validate_promo_code(
     promo_code: Annotated[str, Path()], db: Session = Depends(get_db)
 ):
     return promo_code_exists(promo_code, db)
+
+
+@router.get("")
+def get_promo_codes(db: Session = Depends(get_db)):
+    return get_all_promo_codes(db)
