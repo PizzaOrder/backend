b57e36b36a371f8248f30c116447619c5ce4bdaf
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-11-20 13:41:01 +0300

    feat: add get cafe locations with router

---
+++ crud/cafe_locations_crud.py
@@ -0,0 +1,6 @@
+from sqlalchemy.orm import Session, selectinload
+from core.models.cafe_locations import CafeLocation
+
+
+def get_cafe_locations(db: Session):
+    return db.query(CafeLocation).options(selectinload(CafeLocation.city)).all()
---
+++ routers/cafe_locations_router.py
@@ -0,0 +1,12 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+
+from crud.cafe_locations_crud import get_cafe_locations
+from utils.get_db import get_db
+
+router = APIRouter(prefix="/cafes", tags=["cafe"])
+
+
+@router.get("/")
+async def get_cities(db: Session = Depends(get_db)):
+    return get_cafe_locations(db)
