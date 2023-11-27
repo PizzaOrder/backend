c9bd60e352afd6d651819effce15d993c1fae86a
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-11-28 01:56:14 +0300

    refactor(random_gen_py): add types

--- auth/security/random_code_gen.py
+++ auth/security/random_code_gen.py
@@ -5,7 +5,7 @@ def generate_secret(num_bytes: int = 64) -> str:
     return pyotp.random_base32(num_bytes)
 
 
-def generate_totp(secret):
+def generate_totp(secret: str) -> int:
     totp = pyotp.TOTP(
         secret,
         digits=6,
