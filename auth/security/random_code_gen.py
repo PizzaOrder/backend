cc1184a192802075616ae6837d79a9474ce02cc8
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-11-27 20:16:17 +0300

    feat: add TOTP

---
+++ auth/security/random_code_gen.py
@@ -0,0 +1,14 @@
+import pyotp
+
+
+def generate_secret(num_bytes: int = 64) -> str:
+    return pyotp.random_base32(num_bytes)
+
+
+def generate_totp(secret):
+    totp = pyotp.TOTP(
+        secret,
+        digits=6,
+        interval=1800,
+    )
+    return totp.now()
