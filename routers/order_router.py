1dfe53cc34082fbeb4f4f4264cb843d475c99d49
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-05 23:11:29 +0300

    feat: add order creating(router)

--- routers/order_router.py
+++ routers/order_router.py
@@ -2,15 +2,16 @@ from typing import Annotated
 
 import pendulum
 from fastapi import APIRouter, Body, Depends
+from icecream import ic
 from sqlalchemy.orm import Session
 
 from core.models import Order
 from crud.order_items_crud import create_order_items
 from crud.pizzas_crud import get_pizza, get_pizza_prices
 from crud.promo_code_crud import promo_code_exists
-from middlewares.get_user_middleware import get_user_by_token
-from schemas.order_items_schema import OrderItemBase
-from schemas.orders_schema import OrderBase
+from crud.users_crud import get_user_by_token
+from schemas.order_items_schema import OrderItemBase, OrderItemWithOrderId
+from schemas.orders_schema import OrderBase, OrderInDBBase, OrderOut
 from utils.get_db import get_db
 
 router = APIRouter(prefix="/order", tags=["order"])
@@ -19,11 +20,11 @@ router = APIRouter(prefix="/order", tags=["order"])
 @router.post("/new/")
 def create_new_order(
     order_items: OrderItemBase | list[OrderItemBase],
-    current_user=Depends(get_user_by_token),
-    promo_code=Annotated[str, Body()],
+    token: Annotated[str, Body()],
+    promo_code: Annotated[str, Body()] = None,
     db: Session = Depends(get_db),
 ):
-    create_order_items(order_items, db)
+    current_user = get_user_by_token(token, db)
 
     user_id = current_user.id
     promo_id = (
@@ -35,7 +36,7 @@ def create_new_order(
     if isinstance(order_items, list):
         total_cost = get_pizza_prices([item.pizza_id for item in order_items], db)
     else:
-        total_cost = get_pizza(order_items.pizza_id, db)
+        total_cost = get_pizza(order_items.pizza_id, db).price
     order_time = pendulum.now()
     status = "Создан"
     order_base = OrderBase(
@@ -50,4 +51,22 @@ def create_new_order(
     db.add(order)
     db.commit()
     db.refresh(order)
-    return order
+
+    order = OrderInDBBase(**order.__dict__)
+
+    ic(order)
+    ic(order_items)
+
+    if isinstance(order_items, list):
+        order_items = [
+            OrderItemWithOrderId(**x.model_dump(), order_id=order.id)
+            for x in order_items
+        ]
+    else:
+        order_items = OrderItemWithOrderId(
+            **order_items.model_dump(), order_id=order.id
+        )
+
+    order_items = create_order_items(order_items, db)
+    ic(order_items)
+    return OrderOut(order=order, order_items=order_items)
