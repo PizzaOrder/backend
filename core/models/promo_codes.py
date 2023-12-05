5a3a15218ca48d7cac4f98670db56ae08587e082
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-06 01:54:50 +0300

    feat(PromoCode): code indexed, add img_source

--- core/models/promo_codes.py
+++ core/models/promo_codes.py
@@ -7,7 +7,8 @@ class PromoCode(Base):
     __tablename__ = "promo_codes"
 
     id = Column(Integer, primary_key=True)
-    code = Column(String, unique=True)
+    code = Column(String, unique=True, index=True)
     discount_percentage = Column(Integer)
     start_date = Column(DateTime)
     end_date = Column(DateTime)
+    img_source = Column(String)
