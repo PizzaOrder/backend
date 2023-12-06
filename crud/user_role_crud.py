64c1040c5c9c514d458dcdbd3ab464ee46a131da
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-06 17:03:00 +0300

    fix: get all orders for admins

--- crud/user_role_crud.py
+++ crud/user_role_crud.py
@@ -4,4 +4,8 @@ from core.models import User
 
 
 def is_user_admin(user_id: int, db: Session) -> bool:
-    return db.query(User).options(selectinload(User.user_role)).first() == "Admin"
+    user = (db.query(User).filter_by(id=user_id).
+            options(selectinload(User.user_role)).one())
+    user_role = user.user_role[0].role == 'admin'
+
+    return user_role
--- routers/admin_router.py
+++ routers/admin_router.py
@@ -6,16 +6,14 @@ from sqlalchemy.orm import Session
 from crud.order_crud import get_all_orders_for_admin
 from crud.user_role_crud import is_user_admin
 from crud.users_crud import get_user_by_token
-from schemas.orders_schema import OrderOut
 from utils.get_db import get_db
 
 router = APIRouter(prefix="/admin", tags=["admin"])
 
 
 @router.get("/order/")
-def get_all_orders(
-    token: Annotated[str, Header()], db: Session = Depends(get_db)
-) -> list[OrderOut] | OrderOut:
+def get_all_orders(token: Annotated[str, Header()],
+                   db: Session = Depends(get_db)):
     current_user = get_user_by_token(token, db)
     if is_user_admin(current_user.id, db):
         return get_all_orders_for_admin(db)
