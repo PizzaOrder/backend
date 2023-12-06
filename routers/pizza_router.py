34afa18f4bb2b22dba045e9595b2c8d08bc2696f
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-06 17:38:36 +0300

    refactor(Pizza model): add img_source

--- routers/pizza_router.py
+++ routers/pizza_router.py
@@ -2,12 +2,11 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from crud.pizzas_crud import get_all_pizzas
-from schemas.pizzas_schema import PizzaModel
 from utils.get_db import get_db
 
 router = APIRouter(prefix="/pizzas", tags=["pizza"])
 
 
-@router.get("", response_model=list[PizzaModel])
+@router.get("")
 def get_pizzas(db: Session = Depends(get_db)):
     return get_all_pizzas(db)
