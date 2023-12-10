482e8d58427ca8a6e7070b4b9f515edfb7058044
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-11 01:13:45 +0300

    refactor(cafe_locations_router): add schemas

--- routers/cafe_locations_router.py
+++ routers/cafe_locations_router.py
@@ -8,12 +8,16 @@ from utils.get_db import get_db
 router = APIRouter(prefix="/cafes", tags=["cafe"])
 
 
-@router.get("/", response_model=list[CafeLocationWithCity] | CafeLocationWithCity)
+@router.get(
+    "/", response_model=list[CafeLocationWithCity] | CafeLocationWithCity | None
+)
 def get_cafes(db: Session = Depends(get_db)):
     return get_cafe_locations(db)
 
 
-@router.get("/city/{city_name}",
-            response_model=list[CafeLocationWithCity] | CafeLocationWithCity)
+@router.get(
+    "/city/{city_name}",
+    response_model=list[CafeLocationWithCity] | CafeLocationWithCity | None,
+)
 def get_cities_by_name(city_name: str, db: Session = Depends(get_db)):
     return get_cafes_in_city(city_name, db)
