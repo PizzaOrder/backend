0f06e30a6d228eda799091e73759e02695fcbc04
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-10 18:01:24 +0300

    refactor: add PizzaSchema for router

--- routers/pizza_router.py
+++ routers/pizza_router.py
@@ -2,11 +2,12 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from crud.pizzas_crud import get_all_pizzas
+from schemas.pizzas_schema import PizzaModel
 from utils.get_db import get_db
 
 router = APIRouter(prefix="/pizzas", tags=["pizza"])
 
 
-@router.get("")
+@router.get("", response_model=list[PizzaModel] | PizzaModel)
 def get_pizzas(db: Session = Depends(get_db)):
     return get_all_pizzas(db)
--- schemas/pizzas_schema.py
+++ schemas/pizzas_schema.py
@@ -4,13 +4,14 @@ from pydantic import BaseModel
 class PizzaBase(BaseModel):
     name: str
     price: float
-
-    class Config:
-        from_attributes = True
+    img_source: str
 
 
 class PizzaInDBBase(PizzaBase):
     id: int
+
+    class Config:
+        from_attributes = True
 
 
 class PizzaModel(PizzaInDBBase):
