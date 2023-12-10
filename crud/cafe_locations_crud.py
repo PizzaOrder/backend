ce7c238bae06b03a39dbfe3ddc61e84038f2870d
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-11 00:47:30 +0300

    feat: get cafe locations by city

--- crud/cafe_locations_crud.py
+++ crud/cafe_locations_crud.py
@@ -1,7 +1,20 @@
 from sqlalchemy.orm import Session, selectinload
 
+from core.models import City
 from core.models.cafe_locations import CafeLocation
 
 
 def get_cafe_locations(db: Session):
     return db.query(CafeLocation).options(selectinload(CafeLocation.city)).all()
+
+
+def get_cafes_in_city(city_param: str, db: Session):
+    if city := db.query(City).filter_by(city=city_param).first():
+        return (
+            db.query(CafeLocation)
+            .filter_by(city_id=city.id)
+            # .options(selectinload(CafeLocation.city))
+            .all()
+        )
+    else:
+        return None
--- routers/cafe_locations_router.py
+++ routers/cafe_locations_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from crud.cafe_locations_crud import get_cafe_locations
+from crud.cafe_locations_crud import get_cafe_locations, get_cafes_in_city
 from utils.get_db import get_db
 
 router = APIRouter(prefix="/cafes", tags=["cafe"])
@@ -10,3 +10,8 @@ router = APIRouter(prefix="/cafes", tags=["cafe"])
 @router.get("/")
 def get_cities(db: Session = Depends(get_db)):
     return get_cafe_locations(db)
+
+
+@router.get("/city/{city_name}")
+def get_cities_by_name(city_name: str, db: Session = Depends(get_db)):
+    return get_cafes_in_city(city_name, db)
