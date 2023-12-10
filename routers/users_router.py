8b6918273af8e16ef8e6a7acdf6128e48f9f31f9
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-11 01:29:46 +0300

    refactor(user_router): add schemas

--- routers/users_router.py
+++ routers/users_router.py
@@ -4,20 +4,18 @@ from fastapi import APIRouter, Depends, Header
 from sqlalchemy.orm import Session
 
 from crud.users_crud import get_user_by_token
-from schemas.users_schema import ChangeUserProfile
+from schemas.users_schema import ChangeUserProfile, UserBase
 from utils.get_db import get_db
 
 router = APIRouter(prefix="/user", tags=["users"])
 
 
-@router.get("/me/")
-def get_current_user(
-    token: Annotated[str, Header()], db: Session = Depends(get_db)
-):
+@router.get("/me/", response_model=UserBase)
+def get_current_user(token: Annotated[str, Header()], db: Session = Depends(get_db)):
     return get_user_by_token(token, db)
 
 
-@router.put("/me/")
+@router.put("/me/", response_model=UserBase)
 def update_user_profile(
     token: Annotated[str, Header()],
     user_profile: ChangeUserProfile,
