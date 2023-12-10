39ffc70067f827eb6853ef7c410f28623f565390
Dmitriy Lapin <dima.ladmol@yandex.ru>
2023-12-11 01:00:03 +0300

    refactor(CafeLocation): add schemas

--- routers/cafe_locations_router.py
+++ routers/cafe_locations_router.py
@@ -2,16 +2,18 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from crud.cafe_locations_crud import get_cafe_locations, get_cafes_in_city
+from schemas.cafe_locations_schemas import CafeLocationWithCity
 from utils.get_db import get_db
 
 router = APIRouter(prefix="/cafes", tags=["cafe"])
 
 
-@router.get("/")
-def get_cities(db: Session = Depends(get_db)):
+@router.get("/", response_model=list[CafeLocationWithCity] | CafeLocationWithCity)
+def get_cafes(db: Session = Depends(get_db)):
     return get_cafe_locations(db)
 
 
-@router.get("/city/{city_name}")
+@router.get("/city/{city_name}",
+            response_model=list[CafeLocationWithCity] | CafeLocationWithCity)
 def get_cities_by_name(city_name: str, db: Session = Depends(get_db)):
     return get_cafes_in_city(city_name, db)
